809ff36a128a277a9e5cc80d7c4f99ec3d46647a
Paras Rajain <parasrajain7@gmail.com>
2025-09-01 21:40:04 +0530

    2 commit

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,19 +1,19 @@
-import type { StorybookConfig } from '@storybook/react-vite';
+import type { StorybookConfig } from "@storybook/react-vite";
 
 const config: StorybookConfig = {
-  "stories": [
-    "../src/**/*.mdx",
-    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
-  ],
-  "addons": [
-    "@chromatic-com/storybook",
-    "@storybook/addon-docs",
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+
+  addons: [
+    "@storybook/addon-links",
     "@storybook/addon-a11y",
-    "@storybook/addon-vitest"
+    "@chromatic-com/storybook",
+    "@storybook/addon-docs"
   ],
-  "framework": {
-    "name": "@storybook/react-vite",
-    "options": {}
+
+  framework: {
+    name: "@storybook/react-vite",
+    options: {},
   }
 };
+
 export default config;
